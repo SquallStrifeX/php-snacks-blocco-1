e79aa4546d29f6b08c7257daa36d1b3b9ef74ed7
SquallStrifeX <squallstrifex@gmail.com>
2024-01-24 17:41:22 +0100

    introducing get

--- Snack 2/index.php
+++ Snack 2/index.php
@@ -1,3 +1,16 @@
+<?php 
+
+if (isset($_GET['nome']) && isset($_GET['email']) && isset($_GET['eta'])) {
+    $nome = $_GET['nome'];
+    $email = $_GET['email'];
+    $eta = $_GET['eta'];
+} if (strlen($nome) < 3) &&  {
+        $erroreNome = 'Il nome deve contenere almeno 3 caratteri.';
+    }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,26 +20,34 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="">
+    <form action="index.php" method="$_GET">
         <div class="container mt-4">
             <div class="row">
                 <div class="col-4">
                     <div class="mb-3">
   <label for="exampleFormControlInput1" class="form-label">Nome</label>
-  <input type="text" class="form-control" id="Nome" placeholder="Inserisci il tuo nome">
+  <input type="text" class="form-control" id="Nome" placeholder="Inserisci il tuo nome" name="nome">
+  <small class="text-danger"><?php echo $erroreNome; ?></small>
 </div>
                 </div>
                 <div class="col-4">
                     <div class="mb-3">
   <label for="exampleFormControlInput1" class="form-label">Email</label>
-  <input type="email" class="form-control" id="Email" placeholder="Inserisci la tua email">
+  <input type="email" class="form-control" id="Email" placeholder="Inserisci la tua email" name="email">
 </div>
                 </div>
                 <div class="col-4">
                     <div class="mb-3">
   <label for="exampleFormControlTextarea1" class="form-label">Età</label>
-  <input type="text" class="form-control" id="Eta" placeholder="inserisci la tua età" ></input>
+  <input type="text" class="form-control" id="Eta" placeholder="inserisci la tua età" name="eta" ></input>
 </div>
+                </div>
+                <div class="col-4">
+                 <button class="btn btn-success"> Invia i dati</button>
+</div>
+                </div>
+                <div class="col-4">
+                    <?php echo $nome . $email . $eta ?>
                 </div>
             </div>
         </div>
