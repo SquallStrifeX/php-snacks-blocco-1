5b55824fdd2ade106f30a6dd1ed9383208309b18
SquallStrifeX <squallstrifex@gmail.com>
2024-01-24 17:07:04 +0100

    scaffolding snack 2

---
+++ Snack 2/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-4">
+                    <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Nome</label>
+  <input type="text" class="form-control" id="Nome" placeholder="Inserisci il tuo nome">
+</div>
+                </div>
+                <div class="col-4">
+                    <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email</label>
+  <input type="email" class="form-control" id="Email" placeholder="Inserisci la tua email">
+</div>
+                </div>
+                <div class="col-4">
+                    <div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Età</label>
+  <input type="text" class="form-control" id="Eta" placeholder="inserisci la tua età" ></input>
+</div>
+                </div>
+            </div>
+        </div>
+
+
+
+</form>
+</body>
+</html>
