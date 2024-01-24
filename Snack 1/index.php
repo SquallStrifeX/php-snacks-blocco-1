060e632a56917cef5da558fac53d3fb1d06028ec
SquallStrifeX <squallstrifex@gmail.com>
2024-01-24 16:55:13 +0100

    end snack 1

--- Snack 1/index.php
+++ Snack 1/index.php
@@ -1,3 +1,32 @@
+<?php 
+$squadre = [
+    [
+        'squadra_casa' => 'From Software',
+        'squadra_ospite' => 'Santa Monica Studios',
+        'punti_casa' => '199',
+        'punti_ospite' => '187',
+    ],
+    [
+        'squadra_casa' => 'Naughty Dog',
+        'squadra_ospite' => 'Namco Bandai',
+        'punti_casa' => '216',
+        'punti_ospite' => '143',
+    ],
+    [
+        'squadra_casa' => 'Square-Enix',
+        'squadra_ospite' => 'Ubisoft',
+        'punti_casa' => '175',
+        'punti_ospite' => '151',
+    ],
+    [
+        'squadra_casa' => 'Insomniac',
+        'squadra_ospite' => 'Rockstar',
+        'punti_casa' => '172',
+        'punti_ospite' => '215',
+    ],
+];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +35,9 @@
     <title>Document</title>
 </head>
 <body>
-    
+<?php
+foreach ($squadre as $squadra) {
+    echo $squadra['squadra_casa']. ' - ' . $squadra['squadra_ospite'] . ' | ' . $squadra['punti_casa'].'-'. $squadra['punti_ospite'].'<br>'.'<br>';
+} ?>
 </body>
 </html>
